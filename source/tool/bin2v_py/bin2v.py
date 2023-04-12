c2b5e9287b559ebfd560b2d08904b97512a82702
HRA! <hra@mvj.biglobe.ne.jp>
2023-04-12 22:46:51 +0900

    Updated

--- source/tool/bin2v_py/bin2v.py
+++ source/tool/bin2v_py/bin2v.py
@@ -5,7 +5,7 @@ import sys
 
 # ------------------------------------------------
 def usage( p_name ):
-    print( "Usage> %s <input.bin> <output.v>" % p_name )
+    print( "Usage> %s <input.bin> <output.v> <bits>" % p_name )
 
 # ------------------------------------------------
 def main():
@@ -13,9 +13,11 @@ def main():
 	print( "==========================================================" )
 	print( "(C)2023 t.hara" )
 
-	if( len(sys.argv) < 3 ):
+	if( len(sys.argv) < 4 ):
 		usage( sys.argv[0] )
 		exit(1)
+
+	bits = int( sys.argv[3] )
 
 	with open( sys.argv[1], "rb" ) as ifile:
 		datas = ifile.read()
@@ -23,7 +25,7 @@ def main():
 	with open( sys.argv[2], "w" ) as ofile:
 		address = 0
 		for d in datas:
-			ofile.write( "\t\t11'h%03x:\tff_dbi <= 8'h%02x;\n" % ( address, d ) )
+			ofile.write( "\t\t%d'h%03x:\tff_dbi <= 8'h%02x;\n" % ( bits, address, d ) )
 			address = address + 1
 
 if __name__ == "__main__":
--- source/tool/bin2v_py/test.bat
+++ source/tool/bin2v_py/test.bat
@@ -1,3 +1,3 @@
 bin2v.exe hex_iplrom_304k_rev300.bin hex_iplrom_304k_rev300_ref.v
-python bin2v.py hex_iplrom_304k_rev300.bin hex_iplrom_304k_rev300.v
+python bin2v.py hex_iplrom_304k_rev300.bin hex_iplrom_304k_rev300.v 11
 pause
